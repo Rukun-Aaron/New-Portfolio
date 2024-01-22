2b029022f3c2156e31323aee0aa3081a68e5984f
rukun aaron <raar518@aucklanduni.ac.nz>
2024-01-22 20:06:56 +1300

    added ul

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import ProjectCard from "../sub/ProjectCard";
+import ProjectTag from "../sub/ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    src: "/NextWebsite.png",
+    title: "Modern Next.js Portfolio",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tag: ["All", "Web"]
+  },
+  {
+    id: 2,
+    src: "/CardImage.png",
+    title: "Interactive Website Cards",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tag: ["All", "Web"]
+  },
+  {
+    id: 3,
+    src: "/SpaceWebsite.png",
+    title: "Space Themed Website",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tag: ["All", "Web"]
+  },
+]
 
 const Projects = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag: any) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
   return (
     <div
       className="flex flex-col items-center justify-center py-20 z-[20]"
@@ -10,8 +53,41 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div ref={ref}  className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 px-10 md:gap-12">
+
+        {filteredProjects.map((project: any, index: any) => (
+          <motion.div
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}>
+            <ProjectCard
+              src={project.src}
+              title={project.title}
+              description={project.description}/>
+          </motion.div>
+        ))}
+        </ul>
+        {/* < ProjectCard
           src="/NextWebsite.png"
           title="Modern Next.js Portfolio"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -25,7 +101,7 @@ const Projects = () => {
           src="/SpaceWebsite.png"
           title="Space Themed Website"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
+        /> */}
       </div>
     </div>
   );
